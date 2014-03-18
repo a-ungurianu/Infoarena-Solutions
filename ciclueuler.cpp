c80e54fffccba6680dc856eaad755af1f2839e95
Ungurianu Alexandru <alexandru.ungurianu@gmail.com>
2014-03-18 17:38:00 +0200

    Solved the Eulerian Cycle algorithm

--- ciclueuler.cpp
+++ ciclueuler.cpp
@@ -1,59 +1,108 @@
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <list>
+#include <stack>
+#include <algorithm>
 
 using namespace std;
+ifstream in("ciclueuler.in");
+ofstream out("ciclueuler.out");
 
 class Graph {
 
 public:
-    Graph();
-    vector<unsigned> GetEulerianCycle();
-    void AddEdge(unsigned a,unsigned b);
-    void SetNNodes(size_t n);
+	Graph(size_t n): noNodes(n),_isConnected(-1) {adLs.resize(n);}
+
+	void addEdge(unsigned a,unsigned b) {
+		adLs[a].push_back(b);
+		adLs[b].push_back(a);
+		if(_isConnected == 0) _isConnected = -1;
+	}
+
+	bool isConnected() {
+		if(_isConnected!=-1) {
+			if(_isConnected)
+				return true;
+			return false;
+		}
+
+		vector<bool> passed;
+		passed.resize(noNodes);
+		connDFS(0,passed);
+		for(size_t i = 0; i < noNodes; ++i) {
+			if(!passed[i]){
+				_isConnected = 0;
+				return false;
+			}
+		}
+		return true;
+	}
+
+	bool isEulerian() {
+		if(!isConnected()) return false;
+		for(size_t i = 0; i < noNodes; ++i) {
+			if(adLs[i].size()%2) return false;
+		}
+		return true;
+	}
+
+	vector<unsigned> getEulerCycle(unsigned node) {
+		stack<unsigned> stk;
+		vector<unsigned> cycle;
+		do {
+			euler(node,stk);
+			node = stk.top();
+			stk.pop();
+			cycle.push_back(node);
+		}while(!stk.empty());
+
+		return cycle;
+	}
 private:
-    size_t nNodes;
-    vector<list<unsigned>> adLs;
-    void RemoveEdge(unsigned a,unsigned b);
+	void connDFS(unsigned nd,vector<bool> &passed) {
+		passed[nd] = true;
+		for(const auto &nd2:adLs[nd]) {
+			if(!passed[nd2])
+				connDFS(nd2,passed);
+		}
+	}
+
+	void euler(unsigned node, stack<unsigned> &stk) {
+		while(true) {
+			if(adLs[node].empty()) 
+				break;
+			unsigned to = adLs[node].front();
+			stk.push(node);
+			adLs[node].pop_front();
+			adLs[to].erase(find(adLs[to].begin(), adLs[to].end(), node));
+			node = to;
+		}
+	}
+
+	int _isConnected;
+	vector<list<unsigned>> adLs;
+	size_t noNodes;
 };
 
-Graph::Graph() {
-    nNodes = 0;
-}
-
-void Graph::AddEdge(unsigned a,unsigned b) {
-    adLs[a].push_back(b);
-    adLs[b].push_back(a);
-}
-
-void Graph::SetNNodes(size_t n) {
-    nNodes = n;
-    adLs.resize(n);
-}
-
-void Graph::RemoveEdge(unsigned a,unsigned b) {
-
-}
-
-void ReadData(Graph& g) {
-    ifstream in("ciclueuler.in");
-    size_t n,m;
-    in >> n >> m;
-    g.SetNNodes(n);
-    for(size_t i = 0; i < m; ++i) {
-        unsigned a,b;
-        in >> a >> b;
-        g.AddEdge(a-1,b-1);
-    }
-}
-
 int main() {
-    Graph g;
-    ReadData(g);
+	size_t n,m;
+	in >> n >> m;
+	Graph G(n);
+	for(size_t i = 0; i < m; ++i) {
+		unsigned a,b;
+		in >> a >> b;
+		G.addEdge(a-1,b-1);
+	}
+	if(G.isEulerian()){
 
-    vector<unsigned> cycle = g.GetEulerianCycle();
+		vector<unsigned> cycle = G.getEulerCycle(0);
 
-    for(size_t i = 0,n = cycle.size(); i < n; ++i) {
-        cout << cycle[i]+1 << ' ';
-    }
+		for(size_t i = 0; i < cycle.size(); ++i) {
+	        out << cycle[i]+1 << ' ';
+		}
+		out << '\n';
+	}
+	else 
+		out << "-1\n";
 }
