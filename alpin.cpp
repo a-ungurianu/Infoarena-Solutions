6f44f84ecc43b231b902be87bf70cee465a11a5e
nighthawk702 <alexandru.ungurianu@gmail.com>
2014-12-18 23:35:20 +0200

    100p - Did a bit of refactoring and added comments

--- alpin.cpp
+++ alpin.cpp
@@ -7,88 +7,93 @@ ifstream in("alpin.in");
 ofstream out("alpin.out");
 
 struct Path {
-	Path(size_t _pL,size_t _pC, size_t _len) {
-		pL = _pL;
-		pC = _pC;
-		len = _len;
-	}
-	size_t pL,pC;
-	unsigned len;
+	Path(int line,int column, unsigned length) :
+        line(line),
+        column(column),
+        length(length) {}
+
+    int line;        // The line of the end of the path
+    int column;      // The column of the end of the path
+	unsigned length; // The length of the path that ends in the point above
 };
 
-vector<vector<Path>> best;
-vector<vector<unsigned>> mat;
+template <typename T>
+using Matrix = vector<vector<T>>;
 
-void InitMats(size_t n) {
-	best.resize(n);
-	mat.resize(n);
-	for(size_t i = 0; i < n; ++i) { 
-		best[i].resize(n,Path(0,0,0));
-		mat[i].resize(n,0);
-	}
+unsigned mapSize;
+
+Matrix<Path> bestPathTo;
+
+Matrix<unsigned> mountainMap;
+
+bool InBounds(int i, int j) {
+	return !((i < 0 || i >= mapSize) ||
+             (j < 0 || j >= mapSize));
 }
 
-void ReadMat(size_t n) {
-	for(size_t i = 0; i < n; ++i) {
-		for(size_t j = 0; j < n; ++j) {
-			in >> mat[i][j];
-		}
-	}
-}
+// Possible neighbour deltas for a given positions
+const int dl[] = {-1, 0, 1, 0};
+const int dc[] = { 0, 1, 0,-1};
 
-bool InBounds(size_t i, size_t j, size_t n) {
-	return !((i < 0 || i >= n) || (j < 0 || j >= n));
-}
-
-const int dl[] = {-1,0,1,0};
-const int dc[] = {0,1,0,-1};
-
-Path LongestRoadTo(size_t i, size_t j,size_t n) {
-	if(best[i][j].len > 0) return best[i][j];
+Path LongestPathTo(int i, int j) {
+	if(bestPathTo[i][j].length > 0) return bestPathTo[i][j];
 
 	Path maxPath(i,j,0);
 
-	for(size_t k = 0; k < 4; ++k) {
-		if(InBounds(i+dl[k], j+dc[k], n) && mat[i + dl[k]][j + dc[k]] < mat[i][j]){
-			Path vecPath = LongestRoadTo(i+dl[k], j+dc[k],n);
-			if(vecPath.len+1 > maxPath.len) {
-				maxPath = Path(i+dl[k], j+dc[k], vecPath.len+1);
+	for(int k = 0; k < 4; ++k) {
+		if(InBounds(i + dl[k], j + dc[k]) && // Are we still in the map?
+           mountainMap[i + dl[k]][j + dc[k]] < mountainMap[i][j]) // Are we getting to a lower place?
+        {
+			Path vecPath = LongestPathTo(i + dl[k], j + dc[k]);
+			if(vecPath.length + 1 > maxPath.length) {
+				maxPath = Path(i + dl[k], j + dc[k], vecPath.length+1);
 			}
 		}
 	}
-	best[i][j] = maxPath;
-	return best[i][j];
+	bestPathTo[i][j] = maxPath;
+	return bestPathTo[i][j];
 }
 
 void printPath(Path path) {
-	Path parent = best[path.pL][path.pC];
-	if(parent.pC == path.pC && parent.pL == path.pL) {
-		out << path.pL+1 << ' ' << path.pC + 1 << '\n';
+	Path parent = bestPathTo[path.line][path.column];
+	if(parent.column == path.column && parent.line == path.line) // Is it a start node?
+    {
+		out << path.line + 1 << ' ' << path.column + 1 << '\n';
 	}
 	else {
 		printPath(parent);
-		out << path.pL + 1 << ' ' << path.pC + 1 << '\n';
+		out << path.line + 1 << ' ' << path.column + 1 << '\n';
 	}
 }
 
 int main() {
-	size_t n;
-	in >> n;
-	InitMats(n);
-	ReadMat(n);
+	in >> mapSize;
 
-	Path maxPath(0,0,0);
-	Path outPath(0,0,0);
+    bestPathTo.resize(mapSize);
+    mountainMap.resize(mapSize);
 
+    // Resize line by line
+    for(unsigned i = 0; i < mapSize; ++i) {
+        bestPathTo[i].resize(mapSize,Path(0,0,0));
+        mountainMap[i].resize(mapSize,0);
+    }
 
-	for(size_t i = 0; i < n; ++i) {
-		for(size_t j = 0; j < n; ++j) {
-			if(LongestRoadTo(i,j,n).len > maxPath.len) {
-				maxPath = LongestRoadTo(i,j,n);
-				outPath = Path(i,j,maxPath.len+1);
-			}
+    for(unsigned i = 0; i < mapSize; ++i) {
+        for(unsigned j = 0; j < mapSize; ++j) {
+            in >> mountainMap[i][j];
+        }
+    }
+
+	Path longestPath(0,0,0);
+
+	for(int i = 0; i < mapSize; ++i) {
+		for(int j = 0; j < mapSize; ++j) {
+            Path pathToHere = LongestPathTo(i,j);
+			if(pathToHere.length + 1 > longestPath.length) {
+                longestPath = Path(i,j,pathToHere.length+1);
+            }
 		}
 	}
-	out << outPath.len << '\n';
-	printPath(outPath);
+	out << longestPath.length << '\n';
+	printPath(longestPath);
 }
