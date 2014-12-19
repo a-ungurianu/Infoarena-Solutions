eeffbbabda55145978bc290a90a112310fdfcd10
nighthawk702 <alexandru.ungurianu@gmail.com>
2014-12-19 23:53:51 +0200

    100p - Renamed some variables

--- darb.cpp
+++ darb.cpp
@@ -7,24 +7,25 @@ using namespace std;
 ifstream in("darb.in");
 ofstream out("darb.out");
 
-vector<vector<unsigned>> adLs;
+vector<vector<unsigned>> adjacencyList;
 unsigned last,distToLast;
 
-vector<bool> viz;
+vector<bool> visited;
 
 typedef pair<unsigned,unsigned> BfsNode;
 
 void bfs(unsigned i) {
-	viz[i] = true;
-	queue<BfsNode> Q;
-	for(Q.push(BfsNode(i,0));!Q.empty(); Q.pop()) {
-		BfsNode c = Q.front();
-		last = c.first;
-		distToLast = c.second;
-		for(size_t j = 0; j < adLs[c.first].size(); ++j) {
-			if(!viz[adLs[c.first][j]]) {
-				viz[adLs[c.first][j]] = true;
-				Q.push(BfsNode(adLs[c.first][j],c.second+1));
+	visited[i] = true;
+
+    queue<BfsNode> bfsQueue;
+	for(bfsQueue.push(BfsNode(i, 0)); !bfsQueue.empty(); bfsQueue.pop()) {
+		BfsNode current = bfsQueue.front();
+		last = current.first;
+		distToLast = current.second;
+		for(size_t j = 0; j < adjacencyList[current.first].size(); ++j) {
+			if(!visited[adjacencyList[current.first][j]]) {
+				visited[adjacencyList[current.first][j]] = true;
+				bfsQueue.push(BfsNode(adjacencyList[current.first][j],current.second+1));
 			}
 		}
 	}
@@ -33,21 +34,20 @@ void bfs(unsigned i) {
 int main() {
 	size_t n;
 	in >> n;
-	adLs.resize(n);
-	viz.resize(n,false);
+	adjacencyList.resize(n);
+	visited.resize(n,false);
 	for(size_t i = 0; i < n; ++i) {
 		unsigned a,b;
 		in >> a >> b;
-		adLs[a-1].push_back(b-1);
-		adLs[b-1].push_back(a-1);
+		adjacencyList[a-1].push_back(b-1);
+		adjacencyList[b-1].push_back(a-1);
 	}
 
 	bfs(0);
 	unsigned start = last;
-	viz.resize(0);
-	viz.resize(n,false);
+	visited.resize(0);
+	visited.resize(n,false);
 	bfs(start);
-	unsigned end = last;
 
 	out << distToLast+1 << '\n';
 }
