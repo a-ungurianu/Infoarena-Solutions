001ecbb5204bd1a524428816d1c8c3ec6189d4f1
Alexandru Ungurianu <alexandru.ungurianu@gmail.com>
2015-03-26 11:08:44 +0200

    Resolved for 100p

--- euclid3.cpp
+++ euclid3.cpp
@@ -1,45 +1,27 @@
-#include <stdio.h>
-#include <assert.h>
- 
-inline int gcd( int A, int B, int &X, int &Y )
-{
-    if (B == 0)
-    {
-        X = 1;
-        Y = 0;
-        return A;
-    }
- 
-    int X0, Y0, D;
-    D = gcd( B, A % B, X0, Y0 );
- 
-    X = Y0;
-    Y = X0 - (A / B) * Y0;
-    return D;
-}
- 
-int main()
-{
-    freopen("euclid3.in", "rt", stdin);
-    freopen("euclid3.out", "wt", stdout);
- 
-    int T;
-    for (scanf("%d", &T), assert( T <= 100 ); T; T--)
-    {
-        int A, B, C;
-        scanf("%d %d %d", &A, &B, &C);
- 
-        assert( -1000000000 <= A && A <= 1000000000 );
-        assert( -1000000000 <= B && B <= 1000000000 );
-        assert( -2000000000 <= C && C <= 2000000000 && C != 0 );
-        int D, X, Y;
-        D = gcd( A, B, X, Y );
- 
-        if (C % D)
-            printf("0 0\n");
-        else
-            printf("%d %d\n", X * (C / D), Y * (C / D));
-    }
- 
-    return 0;
+#include <fstream>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	ifstream in("euclid3.in");
+	ofstream out("euclid3.out");
+
+	unsigned t;
+	in >> t;
+	for(unsigned i = 0; i < t; ++i) {
+		int a,b,c;
+		in >> a >> b >> c;
+		EuclidResult res = euclid(a,b);
+		if((c*1.0f)/res.gcd != c/res.gcd) {
+			out << "0 0\n";
+		}
+		else {
+			int rap = c/res.gcd;
+
+			out << res.x*rap << ' ' << res.y*rap << '\n';
+		}
+	}
+	return 0;
 }
